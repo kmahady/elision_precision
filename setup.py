6e4ed1c1461328313215ac881782fe5df696d011
Kyle Mahady <kylemahady@Kyles-MacBook-Air.local>
2022-04-15 09:36:36 -0400

    Added setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='phonetic_distance',
+      version='1.0',
+      description='Calculates phonetic edit distance between two words',
+      author='Kyle Mahady',
+      author_email='kyle_mahady@gmail.com',
+      packages=['phonetic_distance'],
+     )
